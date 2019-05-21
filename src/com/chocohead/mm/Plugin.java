c5acb9e3067e306557aac08d15290ebdbcbde193
Chocohead <Chocohead@Aroma1997.org>
2019-05-21 21:07:13 +0100

    Allow transforming interfaces
    Fixes #2

--- src/com/chocohead/mm/Plugin.java
+++ src/com/chocohead/mm/Plugin.java
@@ -28,8 +28,6 @@ import java.util.function.Predicate;
 
 import org.objectweb.asm.AnnotationVisitor;
 import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.Label;
-import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 
@@ -199,26 +197,13 @@ public class Plugin implements IMixinConfigPlugin {
 
 	static byte[] makeMixinBlob(String name, Collection<? extends String> targets) {
 		ClassWriter cw = new ClassWriter(0);
-		cw.visit(52, Opcodes.ACC_PUBLIC + Opcodes.ACC_SUPER, name, null, "java/lang/Object", null);
+		cw.visit(52, Opcodes.ACC_PUBLIC + Opcodes.ACC_ABSTRACT + Opcodes.ACC_INTERFACE, name, null, "java/lang/Object", null);
 
 		AnnotationVisitor mixinAnnotation = cw.visitAnnotation("Lorg/spongepowered/asm/mixin/Mixin;", false);
 		AnnotationVisitor targetAnnotation = mixinAnnotation.visitArray("value");
 		for (String target : targets) targetAnnotation.visit(null, Type.getType('L' + target + ';'));
 		targetAnnotation.visitEnd();
 		mixinAnnotation.visitEnd();
-
-		MethodVisitor constructor = cw.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null);
-		constructor.visitCode();
-		Label start = new Label();
-		constructor.visitLabel(start);
-		constructor.visitVarInsn(Opcodes.ALOAD, 0);
-		constructor.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
-		constructor.visitInsn(Opcodes.RETURN);
-		Label end = new Label();
-		constructor.visitLabel(end);
-		constructor.visitLocalVariable("this", 'L' + name + ';', null, start, end, 0);
-		constructor.visitMaxs(1, 1);
-		constructor.visitEnd();
 
 		cw.visitEnd();
 		return cw.toByteArray();
@@ -307,5 +292,6 @@ public class Plugin implements IMixinConfigPlugin {
 
 	@Override
 	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+		targetClass.interfaces.remove(mixinClassName.replace('.', '/'));
 	}
 }
